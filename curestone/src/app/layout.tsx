4cac59f21384c2e04a8524e7bd5a713430ccdf51
GaneshB2334 <bastapureganesh21@gmail.com>
2026-07-31 14:44:15 +0530

    Clarity script add

--- curestone/src/app/layout.tsx
+++ curestone/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -240,6 +241,13 @@ export default function RootLayout({
       </head>
       <body className="font-sans min-h-full flex flex-col">{children}</body>
       <GoogleAnalytics gaId="G-DH8MHSNF6C" />
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "xuzdy1huhv");`}
+      </Script>
     </html>
   );
 }
